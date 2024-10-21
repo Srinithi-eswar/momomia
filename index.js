892c0a2e487cea615189776081fba3710221aba4
srini <srinithie123@gmail.com>
2024-10-21 12:12:30 +0530

    second commit

--- index.js
+++ index.js
@@ -31,27 +31,24 @@ $(document).ready(function () {
   });
 
   $(".navbar a").on("click", function (event) {
-    // Make sure this.hash has a value before overriding default behavior
+    
     if (this.hash !== "") {
-      // console.log(this);
-      // console.log(this.hash);
-      // Prevent default anchor click behavior
+    
       event.preventDefault();
-      // Store hash
+     
       var hash = this.hash;
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      
       $("html, body").animate(
         {
           scrollTop: $(hash).offset().top,
         },
         800,
         function () {
-          // Add hash (#) to URL when done scrolling (default click behavior)
+          
           window.location.hash = hash;
         }
       );
-    } // End if
+    } 
   });
 
   $(".homeBtn").click(function (event) {
@@ -65,7 +62,7 @@ $(document).ready(function () {
         },
         800,
         function () {
-          // Add hash (#) to URL when done scrolling (default click behavior)
+          
           window.location.hash = hash;
         }
       );
@@ -230,7 +227,7 @@ $(document).ready(function () {
 });
 
 function openWhatsapp() {
-  // console.log($('#address'));
+  
 
   if ($("#address")[0].value === "") {
     alert("Please Enter Address");
--- save_profile.php
+++ save_profile.php
@@ -1,35 +1,35 @@
 <?php
-// Database connection parameters
-$servername = "localhost";
-$username = "root"; // Default username for XAMPP MySQL
-$password = ""; // Default password for XAMPP MySQL
-$database = "momodb"; // Change to your database name
 
-// Create connection
+$servername = "localhost";
+$username = "root"; 
+$password = ""; 
+$database = "momodb"; 
+
+
 $conn = new mysqli($servername, $username, $password, $database);
 
-// Check connection
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Retrieve form data
+
 $firstname = $_POST['Firstname'];
 $lastname = $_POST['Lastname'];
 $email = $_POST['Email'];
 $password = $_POST['Password'];
 
-// Prepare SQL statement
+
 $sql = "INSERT INTO user_profiles (First_name, Last_name, Email_id, Password)
         VALUES ('$firstname', '$lastname', '$email', '$password')";
 
-// Execute SQL statement
+
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
-// Close connection
+
 $conn->close();
 ?>
